dce5d7ea20a4f8001c4e127a7d133354c79a3e7b
Mohammad Hossein Moradi <moradiweb.ir@gmail.com>
2024-11-08 18:22:57 +0330

    Build File Text_profile.jsx{Design} In Folder Box profile and Add To Box-profile File && Edit Flex In 3 File Image-Me,Box_profil,Text-profile

--- src/components/Box_profile/Box_profile.jsx
+++ src/components/Box_profile/Box_profile.jsx
@@ -1,12 +1,13 @@
 import Image_Me from "./image_Me";
-
+import Text_profile from "./Text_profile";
 export default function Box_Profile(){
 return(
-    <div className="flex justify-center mt-5">
-            <div className="flex flex-row items-center w-9/12 h-96
+    <div className="flex flex-wrap  justify-around  mt-5">
+            <div className="flex  items-center max-md:flex-col justify-around w-9/12 h-96
      bg-boxblue rounded-3xl  shadow shadow-transparent
       delay-50 hover:shadow-GRC transition-all delay-50">
        <Image_Me/>
+       <Text_profile/>
     </div>
     </div>
 
