aa0c382d1484e007a9c0a701984c38a82977ff5f
Mohammad Hossein Moradi <moradiweb.ir@gmail.com>
2024-11-11 02:29:54 +0330

    Add Folder Box_skills and File Box_skills.jsx and div box

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import './App.css'
 import Box_Profile from './components/Box_profile/Box_profile'
+import Box_skills from './components/Box_skills/Box_skills'
 import Header from './components/Header/Header'
 import Menubar from './components/Menubar/Menubar'
 
@@ -11,6 +12,7 @@ function App() {
      <Header/> 
      <Menubar/>
      <Box_Profile/>
+     <Box_skills/>
     </>
   )
 }
---
+++ src/components/Box_skills/Box_skills.jsx
@@ -0,0 +1,12 @@
+export default function Box_skills(){
+    return(
+        <div>
+             <div className="flex flex-wrap  justify-around  mt-5">
+            <div className="flex w-9/12 h-96
+     bg-boxblue rounded-3xl  shadow shadow-transparent
+      delay-50 hover:shadow-GRC transition-all delay-50">
+      </div>
+        </div>
+        </div>
+    )
+}
